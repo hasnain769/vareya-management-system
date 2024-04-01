63ab30ccbfaba086e3df05dfdce42d62a2d7fb3d
syed hasnain <syedhasnain769@gmail.com>
2024-04-02 01:58:10 +0500

    resoved prod errors

--- src/app/api/tracking/postnl/route.ts
+++ src/app/api/tracking/postnl/route.ts
@@ -4,12 +4,12 @@ import { config } from 'dotenv';
 
 config();
 
-export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest) : Promise<any> {
     const API_KEY = process.env.POSTNL_API_KEY;
-    const  customernumber = process.env.CUSTOME_NUMBER // Extracting customer number from path parameter
+    const  customernumber = process.env.CUSTOMER_NUMBER // Extracting customer number from path parameter
     try {
         // Construct the URL with query parameters
-        const postnlTrackingUrlSandbox = `https://api-sandbox.postnl.nl/shipment/v2/status/${customernumber}/updatedshipments`;
+        const postnlTrackingUrlSandbox = `https://api.postnl.nl/shipment/v2/status/${customernumber}/updatedshipments`;
         
         // Construct the query parameters
         const queryParams = {
@@ -34,22 +34,10 @@ export async function GET(req: NextRequest) {
             const data = response.data;
             console.log(data); // Log the data received from the API
             return NextResponse.json(data);
-        } else if (response.status === 400) {
-            // Invalid request
-            throw new Error('Invalid request: ' + response.data.message);
-        } else if (response.status === 401) {
-            // Invalid apikey
-            throw new Error('Invalid apikey: ' + response.data.message);
-        } else if (response.status === 405 || response.status === 429 || response.status === 500) {
-            // Method not allowed, Too many requests, or Internal server error
-            throw new Error('Server error: ' + response.data.message);
-        } else {
-            // Other status codes
-            throw new Error('Unexpected error occurred. Status code: ' + response.status);
-        }
+        } 
     } catch (error) {
         console.error(error);
         // Return an error response
-        return 
+        return error
     }
 }
