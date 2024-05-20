13cef50f55a41a9cd03f3833c3a8b87467eb5da3
syed hasnain <syedhasnain769@gmail.com>
2024-05-20 21:03:00 +0500

    timeout errors testing

--- src/app/api/orderList/route.ts
+++ src/app/api/orderList/route.ts
@@ -3,8 +3,8 @@ import db from '@/database/dbClient';
 import { Order, order } from '@/database/schema';
 import { desc } from 'drizzle-orm';
 
-export const maxDuration = 45; 
-export const dynamic = 'force-dynamic';
+// export const maxDuration = 45; 
+// export const dynamic = 'force-dynamic';
 
 // GET
 export async function GET(request: NextRequest) {
