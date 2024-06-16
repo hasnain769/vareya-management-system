a33ddd36c595a6064fae28e472e8f50680b25599
syed hasnain <syedhasnain769@gmail.com>
2024-06-16 16:53:37 +0500

    new push

--- src/app/(routes)/payments/page.tsx
+++ src/app/(routes)/payments/page.tsx
@@ -2,7 +2,7 @@ import { getAllPaymentsData } from "@/database/dbOperations";
 import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card"
 import { PaymentType } from "@/database/schema";
 
-import Payments from '@/app/screens/payments'
+import Payments from '@/app/screens/Payments'
 async function getPaymentsData() {
   const paymentsData = await getAllPaymentsData();
   console.log(paymentsData)
